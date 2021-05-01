2dbfdeaaa8a06bc0daffc60d7d7ebabc0a7b651a
ChoonKiat <choonkiat.dynp@gmail.com>
2021-05-01 23:50:19 +0800

    add stuff

---
+++ server.py
@@ -0,0 +1,7 @@
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io
