6e5525154de4028e301396ca79215e03305e3120
ChoonKiat <choonkiat.dynp@gmail.com>
2021-05-02 21:15:00 +0800

    added html

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
 from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
-from PIL import Image
+from PIL import Image, ImageColor, ImageDraw, ImageFont, ImageOps
 import numpy as np
-import flask
+import time
+import matplotlib.pyplot as plt
+from flask import Flask, render_template
 import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+model = None
+
+if __name__ == "__main__":
+    app.run(debug=True)
