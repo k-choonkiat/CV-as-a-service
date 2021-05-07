d16bb8ada4c9c0efcea71d7b8d9acb0580881b9a
ChoonKiat <choonkiat.dynp@gmail.com>
2021-05-07 11:50:01 +0800

    added backend

--- methods.py
+++ methods.py
@@ -1,3 +1,17 @@
+import tensorflow as tf
+import time
+import tempfile
+from six.moves.urllib.request import urlopen
+from six import BytesIO
+import numpy as np
+from PIL import Image
+from PIL import ImageColor
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageOps
+import matplotlib.pyplot as plt
+#plt.use('Agg')
+
 
 def download_and_resize_image(url, new_width=256, new_height=256,
                               display=False):
@@ -42,50 +56,84 @@ def draw_bounding_box_on_image(image,
     total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
 
     if top > total_display_str_height:
-    text_bottom = top
+      text_bottom = top
     else:
-    text_bottom = top + total_display_str_height
+      text_bottom = top + total_display_str_height
     # Reverse list and print from bottom to top.
     for display_str in display_str_list[::-1]:
-    text_width, text_height = font.getsize(display_str)
-    margin = np.ceil(0.05 * text_height)
-    draw.rectangle([(left, text_bottom - text_height - 2 * margin),
-                    (left + text_width, text_bottom)],
-                    fill=color)
-    draw.text((left + margin, text_bottom - text_height - margin),
-                display_str,
-                fill="black",
-                font=font)
-    text_bottom -= text_height - 2 * margin
+      text_width, text_height = font.getsize(display_str)
+      margin = np.ceil(0.05 * text_height)
+      draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                      (left + text_width, text_bottom)],
+                      fill=color)
+      draw.text((left + margin, text_bottom - text_height - margin),
+                  display_str,
+                  fill="black",
+                  font=font)
+      text_bottom -= text_height - 2 * margin
 
-
+def load_img(path):
+  img = tf.io.read_file(path)
+  img = tf.image.decode_jpeg(img, channels=3)
+  return img
 
 def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
   """Overlay labeled boxes on an image with formatted scores and label names."""
   colors = list(ImageColor.colormap.values())
-
   try:
     font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
                               25)
   except IOError:
     print("Font not found, using default font.")
     font = ImageFont.load_default()
-
   for i in range(min(boxes.shape[0], max_boxes)):
     if scores[i] >= min_score:
       ymin, xmin, ymax, xmax = tuple(boxes[i])
       display_str = "{}: {}%".format(class_names[i].decode("ascii"),
                                      int(100 * scores[i]))
       color = colors[hash(class_names[i]) % len(colors)]
-      image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-      draw_bounding_box_on_image(
-          image_pil,
-          ymin,
-          xmin,
-          ymax,
-          xmax,
-          color,
-          font,
-          display_str_list=[display_str])
-      np.copyto(image, np.array(image_pil))
+      
+
+      if class_names[i].decode("ascii") == "Car":
+        image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+        draw_bounding_box_on_image(
+            image_pil,
+            ymin,
+            xmin,
+            ymax,
+            xmax,
+            color,
+            font,
+            display_str_list=[display_str])
+        np.copyto(image, np.array(image_pil))
   return image
+    
+def display_image(image):
+  fig = plt.figure(figsize=(20, 15))
+  fig.canvas.draw()
+  img = Image.frombytes('RGB', fig.canvas.get_width_height(),fig.canvas.tostring_rgb())
+  #plt.grid(False)
+  #plt.imshow(image)
+  return img
+
+
+def run_detector(detector, path):
+  img = load_img(path)
+
+  converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+  start_time = time.time()
+  result = detector(converted_img)
+  end_time = time.time()
+
+  result = {key:value.numpy() for key,value in result.items()}
+
+  print("Found %d objects." % len(result["detection_scores"]))
+  print("Inference time: ", end_time-start_time)
+
+  image_with_boxes = draw_boxes(
+      img.numpy(), result["detection_boxes"],
+      result["detection_class_entities"], result["detection_scores"])
+
+  return display_image(image_with_boxes)
+
+
--- server.py
+++ server.py
@@ -1,19 +1,53 @@
-from keras.applications import ResNet50
+
+import tensorflow_hub as hub
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
 from PIL import Image, ImageColor, ImageDraw, ImageFont, ImageOps
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
 import io
+import requests
+from methods import *
 
 app = Flask(__name__)
+
+
+def detect_img(image_url):
+  start_time = time.time()
+  image_path = download_and_resize_image(image_url, 640, 480)
+  res_img = run_detector(detector, image_path)
+  end_time = time.time()
+  print("Inference time:",end_time-start_time)
+  return res_img
 
 @app.route("/")
 def home():
     return render_template("index.html")
-model = None
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    path = request.form['imgName']
+    
+    image = detect_img(path)
+    # return the data dictionary as a JSON response
+    return render_template("index.html",picture=image)
 
 if __name__ == "__main__":
+    module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1" #@param ["https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1", "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"]
+
+    detector = hub.load(module_handle).signatures['default']
+    #resp = urlopen('https://github.com')
+    #print(resp.read())
+    image_urls = [
+    # Source: https://commons.wikimedia.org/wiki/File:The_Coleoptera_of_the_British_islands_(Plate_125)_(8592917784).jpg
+    "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_Coleoptera_of_the_British_islands_%28Plate_125%29_%288592917784%29.jpg",
+    # By Américo Toledano, Source: https://commons.wikimedia.org/wiki/File:Biblioteca_Maim%C3%B3nides,_Campus_Universitario_de_Rabanales_007.jpg
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg/1024px-Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg",
+    # Source: https://commons.wikimedia.org/wiki/File:The_smaller_British_birds_(8053836633).jpg
+    "https://upload.wikimedia.org/wikipedia/commons/0/09/The_smaller_British_birds_%288053836633%29.jpg",
+    ]
+    
     app.run(debug=True)
